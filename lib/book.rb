d03a8e9c4b21225234998d3f5d2b770751c3cca8
djoverbeatz29 <jroverby92@gmail.com>
2020-02-19 14:24:34 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+    attr_accessor :title, :author, :genre, :page_count
 
+    def initialize(title) # , author, genre, page_count)
+        @title = title
+        # @author = author
+        # @genre = genre
+        # @page_count = page_count
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+# lib = Book.new("O Alquimista")
+# lib.author = "Paulo Coelho"
+# lib.genre = "Realismo Magico"
+# lib.page_count = 150
+
+# puts "Title: #{lib.title}\nAuthor: #{lib.author}\nGenre: #{lib.genre}\nPage Count: #{lib.page_count}"
